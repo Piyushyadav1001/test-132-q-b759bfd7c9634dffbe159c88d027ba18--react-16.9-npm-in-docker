2db27989c1ec72e2c9b93fa26bd3ab745c71cc65
Piyushyadav1001 <72632167+Piyushyadav1001@users.noreply.github.com>
2020-11-12 13:27:09 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -35,40 +35,40 @@ function App() {
         <button id="result" onClick={calculate}>
           result
         </button>
-        <button name="+" onClick={handleClick} id="">
+        <button name="+" onClick={handleClick} id="+">
           +
         </button>
-        <button name="7" onClick={handleClick} id="">
+        <button name="7" onClick={handleClick} id="7">
           7
         </button>
-        <button name="8" onClick={handleClick} id="">
+        <button name="8" onClick={handleClick} id="8">
           8
         </button>
-        <button name="9" onClick={handleClick} id="">
+        <button name="9" onClick={handleClick} id="9">
           9
         </button>
-        <button name="-" onClick={handleClick} id="">
+        <button name="-" onClick={handleClick} id="-">
           -
         </button>
-        <button name="4" onClick={handleClick} id="">
+        <button name="4" onClick={handleClick} id="4">
           4
         </button>
-        <button name="5" onClick={handleClick} id="">
+        <button name="5" onClick={handleClick} id="5">
           5
         </button>
-        <button name="6" onClick={handleClick} id="">
+        <button name="6" onClick={handleClick} id="6">
           6
         </button>
-        <button name="*" onClick={handleClick} id="">
+        <button name="*" onClick={handleClick} id="*">
           *
         </button>
-        <button name="1" onClick={handleClick} id="">
+        <button name="1" onClick={handleClick} id="1">
           1
         </button>
-        <button name="2" onClick={handleClick} id="">
+        <button name="2" onClick={handleClick} id="2">
           2
         </button>
-        <button name="3" onClick={handleClick} id="">
+        <button name="3" onClick={handleClick} id="3">
           3
         </button>
         <button name="/" onClick={handleClick} id="/">
